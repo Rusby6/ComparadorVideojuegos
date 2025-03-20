013de295b7624006b1d4c5aa09a076f8d7aa783e
Rusby6 <chyzhruslan@gmail.com>
2025-03-20 22:57:11 +0100

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expón ipcRenderer de manera segura al proceso de renderizado
-contextBridge.exposeInMainWorld('electron', {
-    invoke: (channel, data) => ipcRenderer.invoke(channel, data),
-});
